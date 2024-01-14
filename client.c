113a2f9fb16a9b08a650bd56506d41bb078b0205
Harishankar G <harishankargaddanakeri@gmail.com>
2024-01-15 00:50:16 +0530

    Implementation Complete

--- client.c
+++ client.c
@@ -1,18 +1,49 @@
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <netinet/in.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/poll.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define MAX_BUF 512
+#define MSG_SIZE 513
+#define SNDR_SIZE 19
+
+char QUIT_CMD[] = "QUIT";
+
+typedef struct {
+    char message[MSG_SIZE];
+    char sender[SNDR_SIZE];
+} MSG;
+
+int receive_messages(int client_socket) {
+    MSG recvd_MSG;
+    int bytes_received;
+
+    bytes_received = recv(client_socket, &recvd_MSG, sizeof(MSG), 0);
+    if (bytes_received <= 0) {
+        printf("Server disconnected\n");
+        return -1;
+    }
+
+    printf("%s: %s\n", recvd_MSG.sender, recvd_MSG.message);
+
+    return 0;
+}
+
+int send_message(int client_socket, char message[MSG_SIZE]) {
+    if (send(client_socket, message, strlen(message), 0) == -1) {
+        perror("Message sending failed");
+    }
+
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
     struct sockaddr_in server;
     int client_socket, status;
-    char buffer[MAX_BUF + 1];
+    char buffer[MSG_SIZE];
 
     if (argc < 3) {
         fprintf(stderr, "Usage: client IPADDR PORT\n");
@@ -38,13 +69,41 @@ int main(int argc, char *argv[]) {
         perror("Connection error");
         return EXIT_FAILURE;
     }
+    printf("Connected\n");
+    printf("Use %s to quit\n", QUIT_CMD);
 
-    // Recieve response from the server
-    status = recv(client_socket, buffer, MAX_BUF, 0);
-    buffer[status] = '\0';
+    struct pollfd pfds[] = {{.fd = client_socket, .events = POLLIN}, {.fd = STDIN_FILENO, .events = POLLIN}};
+    while (true) {
+        printf("> ");
+        fflush(stdout);
 
-    // Print the response
-    printf("Recieved from server: %s", buffer);
+        int event_count = poll(pfds, sizeof(pfds) / sizeof(*pfds), -1);
+        if (event_count < 0) {
+            perror("Poll error");
+            return EXIT_FAILURE;
+        }
+
+        if (pfds[0].revents & POLLIN) {
+            status = receive_messages(client_socket);
+            if (status == -1) {
+                break;
+            }
+        }
+
+        if (pfds[1].revents & POLLIN) {
+            fgets(buffer, sizeof(buffer), stdin);
+            buffer[strcspn(buffer, "\n")] = '\0';
+            if (strncmp(buffer, QUIT_CMD, sizeof(QUIT_CMD)) == 0) {
+                break;
+            }
+
+            status = send_message(client_socket, buffer);
+            bzero(buffer, sizeof(buffer));
+            if (status == -1) {
+                break;
+            }
+        }
+    }
 
     close(client_socket);
     return EXIT_SUCCESS;
--- server.c
+++ server.c
@@ -1,5 +1,5 @@
 #include <arpa/inet.h>
-#include <netinet/in.h>
+#include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,22 +7,96 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define MAX_BUF 512
+// 1 extra for null terminator
+#define MSG_SIZE 513
+#define SNDR_SIZE 19
 
-int main(int argc, char *argv[]) {
-    int server_socket, client_socket, port, status, max_connections;
-    struct sockaddr_in server, client; // IPv4 only
-    socklen_t client_length = sizeof(struct sockaddr_in);
-    char *msg = "Hello World\n";
+#define MAX_CLIENTS 10
 
-    if (argc < 3) {
-        fprintf(stderr, "Usage: server PORT MAXCONNECTIONS\n");
-        return EXIT_FAILURE;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct {
+    struct in_addr client_addr;
+    int client_socket;
+} client_info;
+
+typedef struct {
+    char message[MSG_SIZE];
+    char sender[SNDR_SIZE];
+} MSG;
+
+client_info clients[MAX_CLIENTS];
+
+void add_client(client_info client_struct) {
+    pthread_mutex_lock(&mutex);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].client_socket == 0) {
+            clients[i] = client_struct;
+            break;
+        }
     }
 
-    // Process the required input
+    pthread_mutex_unlock(&mutex);
+}
+
+void remove_client(client_info client_struct) {
+    pthread_mutex_lock(&mutex);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].client_socket == client_struct.client_socket) {
+            memset((void *)&clients[i], 0, sizeof(client_info));
+            break;
+        }
+    }
+
+    pthread_mutex_unlock(&mutex);
+}
+
+void broadcast_message(MSG bdcast_msg, int sender) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].client_socket != 0 && clients[i].client_socket != sender) {
+            send(clients[i].client_socket, &bdcast_msg, sizeof(MSG), 0);
+        }
+    }
+}
+
+void *handle_client(void *arg) {
+    client_info client_struct = *(client_info *)arg;
+
+    add_client(client_struct);
+    while (true) {
+        char buffer[MSG_SIZE];
+        int recv_length = recv(client_struct.client_socket, buffer, sizeof(buffer), 0);
+        if (recv_length <= 0) {
+            remove_client(client_struct);
+            printf("Client %s disconnected\n", inet_ntoa(client_struct.client_addr));
+            close(client_struct.client_socket);
+            break;
+        }
+
+        // pack bdcast_msg
+        MSG bdcast_msg;
+        strcpy(bdcast_msg.message, buffer);
+        strcpy(bdcast_msg.sender, inet_ntoa(client_struct.client_addr));
+
+        pthread_mutex_lock(&mutex);
+        broadcast_message(bdcast_msg, client_struct.client_socket);
+        pthread_mutex_unlock(&mutex);
+    }
+
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    int server_socket, client_socket, port, status;
+    struct sockaddr_in server, client; // IPv4 only
+    socklen_t client_length = sizeof(struct sockaddr_in);
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: server PORT\n");
+        return EXIT_FAILURE;
+    }
     port = atoi(argv[1]);
-    max_connections = atoi(argv[2]);
 
     // Fill in the server struct
     memset(&server, 0, sizeof(struct sockaddr_in));
@@ -44,28 +118,27 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    printf("Server listening on port: %d\nMax connections: %d\n", ntohs(server.sin_port), max_connections);
+    printf("Server listening on port: %d\nMax connections: %d\n", ntohs(server.sin_port), MAX_CLIENTS);
 
     // Listen for connections
-    status = listen(server_socket, max_connections);
+    status = listen(server_socket, MAX_CLIENTS);
     if (status == -1) {
         perror("Listen error");
         return EXIT_FAILURE;
     }
 
-    client_socket = accept(server_socket, (struct sockaddr *)&client, &client_length);
-    while (client_socket) {
-        printf("Client %s connected\n", inet_ntoa(client.sin_addr));
-
-        // Send data to client
-        status = send(client_socket, msg, strlen(msg), 0);
-        if (status == -1) {
-            perror("Send error");
+    while (true) {
+        client_socket = accept(server_socket, (struct sockaddr *)&client, &client_length);
+        if (client_socket == -1) {
+            perror("Connect error");
             continue;
         }
+        printf("Client %s connected\n", inet_ntoa(client.sin_addr));
 
-        close(client_socket);
-        client_socket = accept(server_socket, (struct sockaddr *)&client, &client_length);
+        // Create a thread for each client
+        pthread_t client_tid;
+        client_info client_struct = {client.sin_addr, client_socket};
+        pthread_create(&client_tid, NULL, handle_client, &client_struct);
     }
 
     close(server_socket);
